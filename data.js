9dea8dce9da06e5c1c2cff210aa1f03ccaf44e92
coding6622 <palin7036@gmail.com>
2025-10-19 18:05:32 +0330

    data.js

--- data.js
+++ data.js
@@ -4,8 +4,14 @@ const initialPanels = [
         name: "پنل تست",
         description: "shayan878",
         number: 1,
-        link: "https://shayansheykhi.github.io/-shayan878-ah_6622/"
+        link: "https://shayansheykhi.github.io/-shayan878-ah_6622/",
     }
+    {
+        name: "DNS",
+        description: "تلگرام=shayan878",
+        number: 2,
+        link:"https://atlas878.github.io/Gami/",
+            }
 ];
 
 // تابع برای مقداردهی اولیه localStorage

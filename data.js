9cba0c0d5ff9269a48829ad166262e92d70f316a
coding6622 <palin7036@gmail.com>
2025-10-23 12:45:46 +0330

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,33 @@
 // داده‌های اولیه پنل‌ها
 const initialPanels = [
-    {
-        name: "درباره ما",
-        description: "جهت ارتبات با ما روی دکمه ورود ب پنل کلید بفرمایید",
-        number: 1,
-        link: "https://t.me/shayan878"
-    },
-    {
-        name: "اموزش برنامه نویسی",
-        description: "سایت اموزش برنامه نویسی @shayan878",
-        number: 2,
-        link: "https://shayansheykhi.github.io/-shayan878-ah_6622/"
-    },
-    {
-        name: "dns",
-        description: "سای تولید dns@shayan878",
-        number: 3,
-        link: "https://atlas878.github.io/Gami/"
-    }
-    {
-        name: "فست فودی",
-        description: "نمایشی هسته @shayan878",
-        number:5,
-        link: "https://coding6622.github.io/food/"
-    },
+  {
+    name: "درباره ما",
+    description: "جهت ارتباط با ما روی دکمه ورود به پنل کلیک بفرمایید",
+    number: 1,
+    link: "https://t.me/shayan878"
+  },
+  {
+    name: "آموزش برنامه‌نویسی",
+    description: "سایت آموزش برنامه‌نویسی @shayan878",
+    number: 2,
+    link: "https://shayansheykhi.github.io/-shayan878-ah_6622/"
+  },
+  {
+    name: "DNS",
+    description: "سایت تولید DNS @shayan878",
+    number: 3,
+    link: "https://atlas878.github.io/Gami/"
+  },
+  {
+    name: "فست فودی",
+    description: "نمایشی هست @shayan878",
+    number: 5,
+    link: "https://coding6622.github.io/food/"
+  }
 ];
 
 async function fetchPanels() {
-  const response = await fetch("https://coding6622.github.io/al-palin-/<username>/<repo>/main/panels.json");
+  const response = await fetch("https://raw.githubusercontent.com/coding6622/al-palin-/main/panels.json");
   const panels = await response.json();
   return panels;
-}
-    }
 }
